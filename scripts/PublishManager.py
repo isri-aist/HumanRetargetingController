79865070c42de6dab5822eedf99accd587bf0c0a
ThomasDuvinage <toto.duvinage@gmail.com>
2025-06-12 16:09:04 +0900

    migrate(ros2): vive trackers publisher scripts

--- scripts/PublishManager.py
+++ scripts/PublishManager.py
@@ -1,17 +1,17 @@
-import rospy
 from geometry_msgs.msg import PoseStamped
+from rclpy.node import Node
 
-class PublishManager(object):
-    def __init__(self, body_part, msg_type=PoseStamped, topic_prefix="hrc/poses"):
+class PublishManager:
+    def __init__(self, node: Node, body_part: str, msg_type=PoseStamped, topic_prefix="hrc/poses"):
+        self.node = node
         self.body_part = body_part
-        self.pub = rospy.Publisher("{}/{}".format(topic_prefix, body_part), msg_type, queue_size=1)
+        topic_name = f"{topic_prefix}/{body_part}"
+        self.pub = self.node.create_publisher(msg_type, topic_name, 10)
         self.msg = None
 
     def setMsg(self, _msg):
         self.msg = _msg
 
     def publishMsg(self):
-        if self.msg is None:
-            return
-
-        self.pub.publish(self.msg)
+        if self.msg is not None:
+            self.pub.publish(self.msg)
